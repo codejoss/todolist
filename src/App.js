b5cb1001ab27a94c82f1550355a0953ba528eae7
Code Joss <quevedoportas@icloud.com>
2023-08-14 18:16:12 -0600

    :construction: Termina clase 8

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
@@ -6,18 +7,33 @@ import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 
 const defaultTodos = [
-  { text: 'Diseño tarjetas Tete', completed: false },
-  { text: 'Comprar leche', completed: false },
+  { text: 'Diseño tarjetas Tete', completed: true },
   { text: 'Comprar croquetas', completed: true },
+  { text: 'Comprar leche', completed: false },
   { text: 'Estudiar react', completed: false },
   { text: 'Estudiar javascript', completed: false }
 ];
 
 function App () {
+  // Estados
+  // eslint-disable-next-line no-unused-vars
+  const [todos, setTodos] = React.useState(defaultTodos);
+
+  const [searchValue, setSearchValue] = React.useState('');
+  console.log('Log desde App()', searchValue);
+
+  // Estados derivados
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  // Begin component
   return (
     <div className='App'>
-      <TodoCounter complete={7} total={18} />
-      <TodoSearch />
+      <TodoCounter complete={completedTodos} total={totalTodos} />
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
 
       <TodoList>
 
