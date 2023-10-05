62bd10e595c18f1975d8b34e2994aa9e382da8c3
Code Joss <quevedoportas@icloud.com>
2023-10-05 05:37:06 -0600

    :construction: Clase 22 - useContext
    Se implemento useContext en los componentes y se elimino React.Consumer en el componente AppUI

--- src/App/AppUI.jsx
+++ src/App/AppUI.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import { TodoContext } from '../TodoContext';
+
 import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
@@ -6,46 +9,37 @@ import { CreateTodoButton } from '../CreateTodoButton';
 import { TodosLoading } from '../TodosLoading';
 import { TodosError } from '../TodosError';
 import { EmptyTodos } from '../EmptyTodos';
-import { TodoContext } from '../TodoContext';
 
 function AppUI () {
+  const {
+    loading,
+    error,
+    searchedTodos,
+    completeTodo,
+    deleteTodo
+  } = React.useContext(TodoContext);
+
   return (
     <>
       <div className='App'>
-        <TodoCounter
-        // complete={completedTodos}
-        // total={totalTodos}
-        />
+        <TodoCounter />
+        <TodoSearch />
 
-        <TodoSearch
-          // searchValue={searchValue}
-          // setSearchValue={setSearchValue}
-        />
-        <TodoContext.Consumer>
-          {({
-            loading,
-            error,
-            searchedTodos,
-            completeTodo,
-            deleteTodo
-          }) => (
-          <TodoList>
-            { loading && <TodosLoading /> }
-            { error && <TodosError /> }
-            { (!loading && searchedTodos.length === 0) && <EmptyTodos /> }
+        <TodoList>
+          { loading && <TodosLoading /> }
+          { error && <TodosError /> }
+          { (!loading && searchedTodos.length === 0) && <EmptyTodos /> }
 
-            { searchedTodos.map(todo => (
-              <TodoItem
-                key={todo.text}
-                text={todo.text}
-                completed={todo.completed}
-                onComplete={() => completeTodo(todo.text)}
-                onDelete={() => deleteTodo(todo.text)}
-                />
-            ))}
-            </TodoList>
-          )}
-        </TodoContext.Consumer>
+          { searchedTodos.map(todo => (
+            <TodoItem
+              key={todo.text}
+              text={todo.text}
+              completed={todo.completed}
+              onComplete={() => completeTodo(todo.text)}
+              onDelete={() => deleteTodo(todo.text)}
+              />
+          ))}
+          </TodoList>
 
         <CreateTodoButton />
       </div>
--- src/TodoCounter/index.jsx
+++ src/TodoCounter/index.jsx
@@ -1,9 +1,17 @@
 import './TodoCounter.css';
+import React from 'react';
+import { TodoContext } from '../TodoContext';
 
-function TodoCounter ({ total, complete }) {
+function TodoCounter () {
+  const {
+    completedTodos,
+    totalTodos
+  } = React.useContext(TodoContext);
+
   const todosCompletedMessage = 'No hay tareas pendientes';
-  const normalMessage = `Has completado ${complete} de ${total}`;
-  const todoMessage = complete === total ? todosCompletedMessage : normalMessage;
+  const normalMessage = `Has completado ${completedTodos} de ${totalTodos}`;
+
+  const todoMessage = completedTodos === totalTodos ? todosCompletedMessage : normalMessage;
   return (
     <h1>
       {todoMessage}
